7bc87d285b20df6ef745c217a2aae28f26240415
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-04-28 22:24:16 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -2,7 +2,7 @@ variable "prefix" {
   description = "env name"
   type        = string
   sensitive = true
-  
+}
 provider "azuread" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   #version = "=1.4.0"
