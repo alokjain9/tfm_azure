9164cd5326c46387fc2682c457fc6d67491f73d5
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-04-28 22:38:36 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -7,8 +7,13 @@ provider "azuread" {
   client_id       = "eacfbfab-3951-4fa2-8cfd-e9ce11201646"
   client_secret   = "4mwqUcoj8tGl~f6ENcskt7Nc668gDBpej~"
   tenant_id       = "e3352582-d8e6-46e7-bed2-375795d5d764"
+   features {}
 }
-
+#provider "azurerm" {
+ # subscription_id = "37dda7fc-3b29-4ef5-9f12-f4815d036bfd"
+  #tenant_id = "e3352582-d8e6-46e7-bed2-375795d5d764"
+  #features {}
+#}
 
 resource "azurerm_resource_group" "main" {
   name     = "interac-resources"
