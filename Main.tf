570d6c6f6da186047cb1db4d9058fc4894cdcae5
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-04-28 22:29:11 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-#resource "azurerm_public_ip" "pip" {
+resource "azurerm_public_ip" "pip" {
   name                = "interac-pip"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
