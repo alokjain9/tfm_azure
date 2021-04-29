a35bf30c1e89aba7cd60e664b2af459c8d62f696
alokjain9 <56097915+alokjain9@users.noreply.github.com>
2021-04-29 12:06:57 -0400

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -37,7 +37,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "interac-nic1"
+  name                = "interac-nic2"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
